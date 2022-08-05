adb3aa8434e05aba8b5162d117482f7d72aeab9d
Elias-nri <eliasnouri@icloud.com>
2022-08-05 11:44:36 +0100

    création du repo

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+import test from './test.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <test />
+          Hello, World!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function test () {
+    return <h1>test</h1>
+}
+
+export default test
