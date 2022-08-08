2c349d0615b122173ba5d9119ab7aa6e1b5828e5
Elias-nri <eliasnouri@icloud.com>
2022-08-08 15:09:51 +0100

    logo non fini

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Logo from '../img/Logo.png';
+
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+const Navigation = () =>{
+    return (
+        <div className="navigation">
+            <Navbar bg='light' expand="lg">
+                <Nav className="me-auto">
+                    <Nav.Link href="/"><img src={Logo}/></Nav.Link>
+                </Nav>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Navigation;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Navigation from "../components/Navigation";
 
 const Home =() =>{
     return (
-        <p>home</p>
+        <><Navigation /><p>home</p></>
     )
 }
 
